77084fb1015a9059314674df08a471c0553dce2c
scrabwind <scrabwind@gmail.com>
2023-03-05 16:40:57 +0100

    readd dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import dotenv from 'dotenv-safe'
 import { InteractionCreate } from './listeners/interactionCreate.js'
 import { ready } from './listeners/ready.js'
 
-console.log(Object.keys(process?.env))
-
-console.log('xdd')
-
-console.log(process.env.DISCORD_TOKEN)
+dotenv.config()
 
 // Create a new client instance
 import { Client } from 'discord.js'
